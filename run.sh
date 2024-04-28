9d96745646bdbc6bfc86464b512fea04b2e55f3d
calllivecn <calllivecn@outlook.com>
2024-04-28 08:35:26 +0800

    iglk

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 
 
 #python video.py --suffix AIx2 -s 2 --num_process_per_gpu 4 \
-#python video.py --suffix AIx4 -s 4 --encoder hevc_nvenc --num_process_per_gpu 8 \
-#python video-dataloader.py --suffix AIx4 --encoder hevc_nvenc -s 4 --batch-size 16 \
-python video-dataloader.py --suffix AIx2 -s 2 --fp32 --batch-size 16 \
-		-i ~/samba/data/net_disk/media/魔天战神/魔天战神-粤语国语/魔天战神02-大灾难降临.mp4 -o ~/samba/data/net_disk/media/魔天战神/魔天战神-粤语国语-AI放大/
+#python inference_realesrgan_video.py --suffix AIx4 -s 4 \
+#python video-dataloader.py --suffix AIx4 -s 4 --encoder hevc_nvenc --batch-size 4 \
+python video.py --suffix AIx4 -s 4 --encoder hevc_nvenc --num_process_per_gpu 6 \
+		-i ~/samba/data/net_disk/media/魔天战神/魔天战神-日语无字/魔天战神02-大灾难降临.mp4 \
+		-o ~/samba/data/net_disk/media/魔天战神/魔天战神-日语无字-AIx4/
+		#-i ~/samba/data/net_disk/media/宫崎骏/千与千寻/千与千寻.2001.1080p.h265.crf18.mp4 -o ~/samba/data/AI放大-test/
 
 
 exit 0
